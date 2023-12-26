3f080eb86c4ddfbe69531410f8377f539431782e
DevMatheusMonteiro <carlosmathmonteiro@gmail.com>
2023-12-26 18:06:42 -0300

    creating middleware for authenticated token

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,7 +1,33 @@
+import knex from "../database/knex/index.js";
+import AppError from "../utils/AppError.js";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import authConfig from "../configs/auth.js";
+
 export default class SessionsController {
   async create(request, response) {
     const { email, password } = request.body;
 
-    return response.json({ email, password });
+    const user = await knex("users").where({ email }).first();
+
+    if (!user) {
+      throw new AppError("Email e/ou senha incorretos", 401);
+    }
+
+    const { compare } = bcryptjs;
+
+    const passwordMatches = await compare(password, user.password);
+
+    if (!passwordMatches) {
+      throw new AppError("Email e/ou senha incorretos", 401);
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+    const token = jwt.sign({}, secret, {
+      subject: String(user.id),
+      expiresIn,
+    });
+
+    return response.json({ user, token });
   }
 }
